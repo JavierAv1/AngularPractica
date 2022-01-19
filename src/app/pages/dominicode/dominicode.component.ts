6670e3d469da8b947c60e2b61f596ad3caebac27
Javier Avilés <aviles.javier@outlook.com>
2022-01-18 20:00:00 -0600

    Avance de PracticaRedireccionar

---
+++ src/app/pages/dominicode/dominicode.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from "@angular/core";
+@Component({
+    selector: 'app-dominicode',
+    templateUrl:'donminicode.component.html',
+    styleUrls:['dominicodecomponent.css']
+})
+
+export class DominicodeComponent implements OnInit{
+    constructor(){
+
+    }
+    ngOnInit(): void {
+        
+    }
+}
